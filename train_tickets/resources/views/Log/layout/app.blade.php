237e7b50671cd83de1f23714f0480cc1e089aac7
LuciferFromDoomsday <48617655+LuciferFromDoomsday@users.noreply.github.com>
2020-11-16 23:39:36 +0600

    s

--- train_tickets/resources/views/Log/layout/app.blade.php
+++ train_tickets/resources/views/Log/layout/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
 
     <!-- Main css -->
+    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/log.css">
 </head>
 <body>
